b59c3a4357fc9c1ecc9a4967a7bebd86f60c5d0e
Federico Barbieri <federicobarbieri54@gmail.com>
2022-11-20 00:07:26 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -159,3 +159,33 @@ animate();
 //  green.play();
 //}
 //}
+
+// fetching videos from wordpress
+
+
+
+const endpoint = "https://federicobarbieri.dk/cph-cartel/wp-json/wp/v2/media";
+
+
+async function getData() {
+    let result = await fetch(endpoint);
+    showPosts(await result.json());
+}
+
+function showPosts(posts){
+    
+    posts.forEach((bikini) => {
+        
+
+        let template = document.querySelector('template').content;
+        let clone = template.cloneNode(true);
+
+        clone.querySelector('video').src = bikini.source_url;
+
+        let parent = document.querySelector('.animation');
+
+        parent.appendChild(clone);
+    });
+}
+
+getData();
